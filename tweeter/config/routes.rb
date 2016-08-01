73c05ac38e46ddfbe818aa8717861bc0fd6a9525
Katherine Haney <katmhaney@gmail.com>
2016-08-01 19:08:02 -0400

    generated Pages controller with routes

--- tweeter/config/routes.rb
+++ tweeter/config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+
+  get 'pages/home' => 'pages#home'
+
+  get 'pages/profile' => 'pages#profile'
+
+  get 'pages/explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
